4c11defce71ec88aabb73d0f5e16e73660507a78
Kroese <kroese@users.noreply.github.com>
2026-05-31 00:57:54 +0200

    feat: Graceful shutdown (#9)

--- src/entry.sh
+++ src/entry.sh
@@ -17,6 +17,7 @@ cd /run
 . network.sh    # Initialize network
 . boot.sh       # Configure boot
 . proc.sh       # Initialize processor
+. power.sh      # Configure shutdown
 . memory.sh     # Check available memory
 . balloon.sh    # Initialize ballooning
 . config.sh     # Configure arguments
@@ -27,4 +28,12 @@ trap - ERR
 version=$(qemu-system-x86_64 --version | head -n 1 | cut -d '(' -f 1 | awk '{ print $NF }')
 info "Booting ${APP}${BOOT_DESC} using QEMU v$version..."
 
-exec qemu-system-x86_64 ${ARGS:+ $ARGS}
+{ qemu-system-x86_64 ${ARGS:+ $ARGS} >"$QEMU_OUT" 2>"$QEMU_LOG"; rc=$?; } || :
+(( rc != 0 )) && error "$(<"$QEMU_LOG")" && exit 15
+
+terminal
+tail -fn +0 "$QEMU_LOG" --pid=$$ 2>/dev/null &
+cat "$QEMU_TERM" 2> /dev/null & wait $! || :
+
+sleep 1 & wait $!
+[ ! -f "$QEMU_END" ] && finish 0
